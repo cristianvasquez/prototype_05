f753fb15d415595e2ae70a49ecfe3296cb4cb082
Cristian Vasquez <cristianvasquez@gmail.com>
2021-02-02 01:07:01 +0100

    The animal open his eyes...

--- src/main.py
+++ src/main.py
@@ -11,19 +11,11 @@ from misc import label_to_path
 
 THIS_DIR = os.getcwd()
 
-
 def main(
         # input_dir='/home/cvasquez/obsidian/public-garden',
         input_dir='../test_obsidian_vault',
-        output_dir='../output',
 ):
     input_dir = os.path.abspath(os.path.join(THIS_DIR, input_dir))
-    output_dir = os.path.abspath(os.path.join(THIS_DIR, output_dir))
-
-    shutil.rmtree(output_dir)
-
-    # Output dir
-    os.makedirs(output_dir, exist_ok=False)
 
     # Inverted index, used to handle Obsidian's 'shortest path' strategy
     exclude = {'.git', '.obsidian', '.trash'}
@@ -34,40 +26,28 @@ def main(
     for root, dirs, files in os.walk(input_dir, topdown=True):
         dirs[:] = [d for d in dirs if d not in exclude]
 
-        target_path = root.replace(input_dir, output_dir)
-        os.makedirs(target_path, exist_ok=True)
-
         for file_name in files:
             source_file = os.path.join(root, file_name)
-            name, extension = os.path.splitext(file_name)
+            name, _ = os.path.splitext(file_name)
+            with codecs.open(source_file, 'r', encoding='utf-8') as f:
+                fm = frontmatter.load(f)
+                content = fm.content
+                metadata = fm.metadata
 
-            # Copy things that are not markdown
-            if not extension == '.md':
-                target_file = os.path.join(target_path, file_name)
-                copyfile(source_file, target_file)
-            else:
-                # Otherwise generate an HTML
+                links = []
+                for label in get_links(content):
+                    link = get_path(label)
+                    if link is not None:
+                        links.append(link)
 
-                name, _ = os.path.splitext(file_name)
-                with codecs.open(source_file, 'r', encoding='utf-8') as f:
-                    fm = frontmatter.load(f)
-                    content = fm.content
-                    metadata = fm.metadata
-
-                    links = []
-                    for label in get_links(content):
-                        link = get_path(label)
-                        if link is not None:
-                            links.append(link)
-
-                    node = {
-                        'title': get_title(metadata, content, source_file),
-                        'source_file': source_file,
-                        'metadata': metadata,
-                        'content': content,
-                        'links': links
-                    }
-                    print(node)
+                node = {
+                    'title': get_title(metadata, content, source_file),
+                    'source_file': source_file,
+                    'metadata': metadata,
+                    'content': content,
+                    'links': links
+                }
+                print(node)
     # with open(target_filename, 'w', encoding="utf-8") as out_file:
     #     out_file.write(template.render(context))
 
